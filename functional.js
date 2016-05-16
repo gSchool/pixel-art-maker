475cd4664b3a539284c3eb0b68536d48bbe66472
Jeremy <jkaffrey@gmail.com>
2016-05-16 13:27:28 -0600

    basics working

--- functional.js
+++ functional.js
@@ -1,16 +1,40 @@
 'use strict';
 
-var width = 10;
-var height = 10;
+var width = 40;
+var height = 30;
 
-$( document ).ready() {
+var currentColor = "#09e9db";
 
+function drawTable() {
+
+  console.log("Called");
   for (var i = 0; i < width; i++) {
+
+    var divRow = document.createElement('div');
+    divRow.id = 'column' + i;
+    divRow.className = 'columns';
+    document.body.appendChild(divRow);
+
     for (var j = 0; j < height; j++) {
 
-        var daDiv = document.createElement('div');
-        daDiv.id = i + ":" + j;
-        document.body.appendChild(daDiv);
+      var daDiv = document.createElement('div');
+      daDiv.id = i + ":" + j;
+      daDiv.className = "unSelected";
+
+      document.getElementById('column' + i).appendChild(daDiv);
     }
+
+    //document.getElementById('drawingBoard').appendChild(document.createElement('br'));
   }
 }
+
+$(document).on("click", function (event) {
+
+  if (event.target.parentNode.className.indexOf("column") >= 0) {
+
+    event.target.style.backgroundColor = currentColor;
+    event.target.style.border = "1px solid" + currentColor;
+  }
+});
+
+drawTable();
