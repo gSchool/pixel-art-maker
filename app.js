ec98da8ea2a6e256f945d77552bfd00936619fd0
jlhelwig <jeff.helwig@gmail.com>
2017-02-24 12:06:23 -0600

    latest js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 'use strict';
 
-window.onLoad = function (){
-
+window.onload = function (){
+console.log("booyah");
 var divElem = document.createElement('div');
 var canvas = document.getElementById('canvas');
-canvas.append('divElem')
-divElem.style.backgroundColor="red";
-divElem.style.heigth="10%";
+canvas.append(divElem);
+divElem.style.border="1px solid black";
+divElem.style.height="10%";
 divElem.style.width="10%";
 
 
-
+// var currentColorDiv = document.getElementsByClassName('colorRed')[0].style
+// console.log(currentColor);
+//
+// var style = currentColorDiv.getComputedStyle(backgroundColor);
+// console.log("style " + style)
 
 };
