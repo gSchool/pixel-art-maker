e5bd09d467e281f3779da5000f92069e3a5e3f8d
Jules Perryman <jules.perryman@gmail.com>
2017-02-24 16:31:01 -0700

    Initial draft Pixel Art Maker

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+
+
+var container = document.querySelector('.container')
+
+container.addEventListener('click', function(event) {
+	console.log(event.target)
+  if (event.target.classList.contains('cell')) {
+  	event.target.classList.toggle('colored')
+  }
+})
+
+var cellCount = 100
+for (var i = 1; i <= cellCount; i++) {
+  var newCell = document.createElement('div')
+  newCell.classList.add('grid')
+  newCell.textContent = i
+  container.appendChild(newCell)
+}
+
+
+var color = "#ffffff";
+
+var paletteBlue = document.querySelector("#blue");
+var paletteRed = document.querySelector("#red");
+var paletteGreen = document.querySelector("#green");
+
+paletteBlue.addEventListener("click", function(){
+ color = "#2462ab ";
+ console.log("blue");
+})
+
+paletteRed.addEventListener("click", function(){
+ color = "#e32020 ";
+ console.log("red");
+
+})
+
+paletteGreen.addEventListener("click", function(){
+ color = "#2ae831";
+ console.log("green");
+
+})
+
+var container = document.getElementsByClassName('container');
+for (let i = 0; i <gridBoxes.length; i++){
+ document.getElementsByClassName('grid')[i].addEventListener( 'click', function() {
+   document.getElementsByClassName('grid')[i].style.backgroundColor = color;
+ })
+}
