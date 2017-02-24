28403524d4eb5e4e1fffe97474700bfc42526361
jlhelwig <jeff.helwig@gmail.com>
2017-02-24 11:12:09 -0600

    one red div

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 window.onLoad = function (){
 
+var divElem = document.createElement('div');
+var canvas = document.getElementById('canvas');
+canvas.append('divElem')
+divElem.style.backgroundColor="red";
+divElem.style.heigth="10%";
+divElem.style.width="10%";
 
 
 
 
 
-
-
-}
+};
