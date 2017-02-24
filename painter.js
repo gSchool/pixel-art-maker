407c7d3fa04d91b864f6182f3ccc9dd4e6aac32e
Taylor Roberts <taylorlroberts7@gmail.com>
2017-02-24 16:13:48 -0700

    Increased size of grid and color palette

--- painter.js
+++ painter.js
@@ -1,3 +1,13 @@
+var cellCount = 441;
+
+for (var i =0; i<cellCount; i++) {
+  var cell = document.createElement('div');
+  cell.classList.add('square');
+  // cell.className = 'square';
+  document.querySelector('.grid').appendChild(cell);
+}
+
+
 var color = 'black';
 
 var paintBrush = document.querySelector('.palette');
