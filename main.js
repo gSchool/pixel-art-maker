f02d41f77568c7524b8aacb1e9e7b06378402e00
lrobinsonco <lrobinsonco@gmail.com>
2017-02-24 13:22:31 -0700

    variables for sections

---
+++ main.js
@@ -0,0 +1,9 @@
+var sectionOne= document.querySelector('.one');
+
+sectionOne.querySelector(".first").style.backgroundColor = "red";
+sectionOne.querySelector(".second").style.backgroundColor = "green";
+
+var sectionTwo=
+document.querySelector('.two');
+sectionTwo.querySelector(".third").style.backgroundColor = "green";
+sectionTwo.querySelector(".fourth").style.backgroundColor = "red";
