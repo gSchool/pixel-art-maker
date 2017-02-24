1dfa15d80c32bf81a27f1c464b8c4badab0a1759
Lena Ripple <ripplelena@gmail.com>
2017-02-24 13:49:35 -0700

    color palette added

---
+++ index.js
@@ -0,0 +1,14 @@
+var clicker = document.querySelectorAll('section div')
+
+
+
+
+// Tutorial on Event Delegation (Chad)
+
+// var container = document.querySelector(‘.container’)
+// container.addEventListener('click', function(event) {
+//   console.log(event.target)
+//   if (event.target.classList.contains('cell')) {
+//     event.target.classList ;
+//   }
+// })
