54e6bd5887352d8aeabfca24f9ddc4a4badaa8ad
Lena Ripple <ripplelena@gmail.com>
2017-02-24 14:25:37 -0700

    DOM to click red

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var clicker = document.querySelectorAll('section div')
+var grid=document.querySelector('.grid')
+
+grid.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = 'red'
+})
 
 
-
-
-// Tutorial on Event Delegation (Chad)
-
-// var container = document.querySelector(‘.container’)
+// // Tutorial on Event Delegation (Chad)
+// var container = document.querySelector('.container')
+//
 // container.addEventListener('click', function(event) {
-//   console.log(event.target)
+// 	console.log(event.target)
 //   if (event.target.classList.contains('cell')) {
-//     event.target.classList ;
+//   	event.target.classList.toggle('colored')
 //   }
 // })
+//
+// var cellCount = 100
+// for (var i = 1; i <= cellCount; i++) {
+//   var newCell = document.createElement('div')
+//   newCell.classList.add('cell')
+//   newCell.textContent = i
+//   container.appendChild(newCell)
+// }
