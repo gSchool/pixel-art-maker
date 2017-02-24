49344b218e1ad2b8ca7b44c5b0456e87d17a0953
Nick McKendry <nickmckendry@gmail.com>
2017-02-24 15:21:04 -0700

    IT WORKSSSSSSS

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+var artColor = "#fff"
 
+var paint = document.querySelector('.pallette_row')
+
+paint.addEventListener('click', function(event){
+    var targetStyle = window.getComputedStyle(event.target)
+    var color = targetStyle.getPropertyValue("background-color")
+    console.log(color)
+    artColor = color
+    console.log('current color')
+    console.log(color)
+    console.log('current artcolor')
+    console.log(artcolor)
+})
+ art.addEventListener('click', function(event) {
+   console.log(event.target)
+   if (event.target.classList.contains('pixel')) {
+     console.log('its a pixel')
+     event.target.style.backgroundColor = artColor;
+
+   }
+ })
