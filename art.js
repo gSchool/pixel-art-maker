c60479803c31c9c0c6acd1e1145da8e763709bfe
Derek Kramer <derek.kramer88@gmail.com>
2017-02-24 16:18:01 -0700

    Almost finished...

--- art.js
+++ art.js
@@ -9,23 +9,19 @@ var height = width;
 
 for (var i = 0; i < x * y; i++) {
     var newCell = document.createElement('div');
+    newCell.classList.add('cell');
     newCell.style.width = width;
     newCell.style.paddingTop = height;
     container.appendChild(newCell);
 }
 
 container.addEventListener('click', function(event) {
-    event.target.style.backgroundColor = color;
-    // if(event.target.classList.contains('cell')){
-    //     event.target.classList.toggle('colored')
-    // }
+    if(event.target.classList.contains('cell')){
+        event.target.style.backgroundColor = color;
+    }
 });
 
 var palette = document.querySelector(".paletteContainer");
 palette.addEventListener('click', function(event) {
     color = event.target.style.backgroundColor;
-    console.log(event.target.style.backgroundColor);
-    // if(event.target.classList.contains('cell')){
-    //     event.target.classList.toggle('colored')
-    // }
 });
