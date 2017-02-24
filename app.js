187a336d02b332da155bc4b2fd43535207efd50b
Jon Ramer <jramer87@gmail.com>
2017-02-24 15:43:12 -0600

    added color box

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 'use strict';
 
 window.onload = function() {
-  var canvas = document.getElementById('canvas');
-  var colorButtons = document.getElementsByClassName("commonStyle");
-  var currentColor = '';
+    var canvas = document.getElementById('canvas');
+    var colorButtons = document.getElementsByClassName("commonStyle");
+    var currentColor = '';
+    var colorBox = document.getElementById("currentColor");
 
+    for (var i = 0; i < 363; i++) {
+        var divElem = document.createElement('div');
+        canvas.append(divElem);
+        divElem.style.border = "1px solid gray";
+        divElem.style.height = "9%";
+        divElem.style.width = "3%";
+        divElem.style.float = "left";
 
-  for (var i = 0; i < 363; i++) {
-      var divElem = document.createElement('div');
-      canvas.append(divElem);
-      divElem.style.border = "1px solid gray";
-      divElem.style.height = "9%";
-      divElem.style.width = "3%";
-      divElem.style.float = "left";
+        divElem.addEventListener("click", (event) => {
+            event.target.style.backgroundColor = currentColor;
+        });
+    }
 
-      divElem.addEventListener("click", (event) => {
-          event.target.style.backgroundColor = currentColor;
-      });
-  }
-
-  for (let i = 0; i < colorButtons.length; i++) {
-      colorButtons[i].addEventListener("click", function(event) {
-          currentColor = getComputedStyle(colorButtons[i])
-              .backgroundColor;
-      });
-      colorButtons[i].addEventListener("click", function() {
-          console.log("hey");
-      });
-  }
+    for (let i = 0; i < colorButtons.length; i++) {
+        colorButtons[i].addEventListener("click", function(event) {
+            currentColor = getComputedStyle(colorButtons[i])
+                .backgroundColor;
+            colorBox.style.backgroundColor = currentColor;
+        });
+    }
 };
