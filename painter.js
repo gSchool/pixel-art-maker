2848b0525278e8686094a3708c23eb58d8fd6a9a
Taylor Roberts <taylorlroberts7@gmail.com>
2017-02-24 14:57:26 -0700

    Figured out how to store palette color in variable

--- painter.js
+++ painter.js
@@ -1,5 +1,15 @@
+var color = 'black';
+
+var paintBrush = document.querySelector('.palette');
+
+paintBrush.addEventListener('click', function(event) {
+  var target = window.getComputedStyle(event.target);
+  color = target.getPropertyValue("background-color");
+  console.log(color);
+})
+
 var clicker = document.querySelector('.grid');
 
 clicker.addEventListener('click', function(event) {
-  event.target.style.backgroundColor = "red";
+  event.target.style.backgroundColor = color;
 })
