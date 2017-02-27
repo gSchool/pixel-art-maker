94ba752c154e832d27f0fb9dd344f126c00781ea
Jackson Donovan <iinventedtwitter@gmail.com>
2017-02-26 18:19:57 -0700

    only one winner now

--- tacToeScripting.js
+++ tacToeScripting.js
@@ -31,101 +31,124 @@ bod.appendChild(resetButton);
 var des = document.createElement('p');
 des.textContent = "Current Player: " + currentPlayer;
 bod.appendChild(des);
-
-for(var z=0;z<allCells.length;z++){
-  allCells[z].addEventListener('click',function(event){
-    if(event.target.textContent === ""){
-      event.target.textContent = currentPlayer;
-      if(currentPlayer === "X"){
-        currentPlayer = "O";
-        des.textContent = "Current Player: " + currentPlayer;
+var goAgain = document.createElement('h3');
+var gamePlay = true;
+if(gamePlay){
+  for(var z=0;z<allCells.length;z++){
+    allCells[z].addEventListener('click',function(event){
+      if(event.target.textContent === ""){
+        event.target.textContent = currentPlayer;
+        if(currentPlayer === "X"){
+          currentPlayer = "O";
+          des.textContent = "Current Player: " + currentPlayer;
+        }
+        else{
+          currentPlayer = "X";
+          des.textContent = "Current Player: " + currentPlayer;
+        }
       }
-      else{
-        currentPlayer = "X";
-        des.textContent = "Current Player: " + currentPlayer;
+      else if(event.target.textContent !== ""){
+        warning.textContent = "Space already taken!"
+        bod.appendChild(warning);
       }
-    }
-    else if(event.target.textContent !== ""){
-      warning.textContent = "Space already taken!"
-      bod.appendChild(warning);
-    }
-    winnerO.setAttribute('class','win');
-    winner.setAttribute('class','win');
-    if(allCells[0].textContent === "X" && allCells[1].textContent === "X" && allCells[2].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[0].textContent === "X" && allCells[4].textContent === "X" && allCells[8].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[0].textContent === "X" && allCells[3].textContent === "X" && allCells[6].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[1].textContent === "X" && allCells[4].textContent === "X" && allCells[7].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[2].textContent === "X" && allCells[5].textContent === "X" && allCells[8].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[3].textContent === "X" && allCells[4].textContent === "X" && allCells[5].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[6].textContent === "X" && allCells[7].textContent === "X" && allCells[8].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[6].textContent === "X" && allCells[4].textContent === "X" && allCells[2].textContent === "X"){
-      winner.textContent = "X wins!";
-      bod.appendChild(winner);
-    }
-    if(allCells[0].textContent === "O" && allCells[1].textContent === "O" && allCells[2].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[0].textContent === "O" && allCells[4].textContent === "O" && allCells[8].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[0].textContent === "O" && allCells[3].textContent === "O" && allCells[6].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[1].textContent === "O" && allCells[4].textContent === "O" && allCells[7].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[2].textContent === "O" && allCells[5].textContent === "O" && allCells[8].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[3].textContent === "O" && allCells[4].textContent === "O" && allCells[5].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[6].textContent === "O" && allCells[7].textContent === "O" && allCells[8].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    if(allCells[6].textContent === "O" && allCells[4].textContent === "O" && allCells[2].textContent === "O"){
-      winnerO.textContent = "O wins!";
-      bod.appendChild(winnerO);
-    }
-    var accum = 0;
-    for(var n = 0; n < allCells.length; n++){
-      if(allCells[n].textContent !== ""){
-        accum++;
+      winnerO.setAttribute('class','win');
+      winner.setAttribute('class','win');
+      if(allCells[0].textContent === "X" && allCells[1].textContent === "X" && allCells[2].textContent === "X"){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
       }
-    }
-    if(accum === 9 && winnerO.textContent !== "O wins!" && winner.textContent !== "X wins!"){
-      tieMessage.textContent = "Tie! Press the reset buttom to go again!";
-      bod.appendChild(tieMessage);
-    }
-  })
+      if(allCells[0].textContent === "X" && allCells[4].textContent === "X" && allCells[8].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[0].textContent === "X" && allCells[3].textContent === "X" && allCells[6].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[1].textContent === "X" && allCells[4].textContent === "X" && allCells[7].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[2].textContent === "X" && allCells[5].textContent === "X" && allCells[8].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[3].textContent === "X" && allCells[4].textContent === "X" && allCells[5].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[6].textContent === "X" && allCells[7].textContent === "X" && allCells[8].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[6].textContent === "X" && allCells[4].textContent === "X" && allCells[2].textContent === "X" && gamePlay){
+        winner.textContent = "X wins!";
+        bod.appendChild(winner);
+        gamePlay = false;
+      }
+      if(allCells[0].textContent === "O" && allCells[1].textContent === "O" && allCells[2].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[0].textContent === "O" && allCells[4].textContent === "O" && allCells[8].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[0].textContent === "O" && allCells[3].textContent === "O" && allCells[6].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[1].textContent === "O" && allCells[4].textContent === "O" && allCells[7].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[2].textContent === "O" && allCells[5].textContent === "O" && allCells[8].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[3].textContent === "O" && allCells[4].textContent === "O" && allCells[5].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[6].textContent === "O" && allCells[7].textContent === "O" && allCells[8].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      if(allCells[6].textContent === "O" && allCells[4].textContent === "O" && allCells[2].textContent === "O" && gamePlay){
+        winnerO.textContent = "O wins!";
+        bod.appendChild(winnerO);
+        gamePlay = false;
+      }
+      var accum = 0;
+      for(var n = 0; n < allCells.length; n++){
+        if(allCells[n].textContent !== ""){
+          accum++;
+        }
+      }
+      if(accum === 9 && winnerO.textContent !== "O wins!" && winner.textContent !== "X wins!"){
+        tieMessage.textContent = "Tie! Press the reset buttom to go again!";
+        bod.appendChild(tieMessage);
+      }
+    })
+  }
+}
+else{
+  goAgain.textContent = "Press the reset button";
+  bod.appendChild(goAgain);
 }
 resetButton.addEventListener('click',function(event){
   for(var y = 0; y<allCells.length;y++){
@@ -137,4 +160,5 @@ resetButton.addEventListener('click',function(event){
   winnerO.textContent = "";
   warning.textContent = "";
   tieMessage.textContent = "";
+  gamePlay = true;
 })
