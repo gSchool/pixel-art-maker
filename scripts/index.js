abe8490642c8481a31a3a18da0b5986fd8d35b41
Sean McDermott <mcderm.sean@gmail.com>
2017-03-04 13:55:49 -0800

    removed console.log to further optimize cell-creating algorithm

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 function createCanvas(rowSize, columnSize) {
   let pixelWidth = (100 / rowSize)
   for (var i = 0; i < rowSize * columnSize; i++) {
-    console.log(pixelWidth);
     $('div.canvas').append('<div class="pixel"></div>');
   };
   $('div.pixel').css('width', `${pixelWidth}%`)
 };
 
 $(document).ready(function () {
-  createCanvas(50, 25);
+  createCanvas(200, 200);
 });
