a24afccd62d4fad86f87e3003d9e8e941c4e0d94
Sean McDermott <mcderm.sean@gmail.com>
2017-03-04 13:05:26 -0800

    able to make a 2x2 table of square divs, hard coded to 2 columns

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 console.log('sanity check');
-// let $px = $('div.canvas').html('<div>stuff</div>')
+
 $(document).ready(function () {
   for (var i = 0; i < 4; i++) {
     console.log(i)
-    $('div.canvas').append('<div class="pixel">stuff</div>')
+    $('div.canvas').append('<div class="pixel"></div>')
   }
 })
