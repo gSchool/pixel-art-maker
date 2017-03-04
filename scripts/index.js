d784beba1c258fa0fd91311c0084566b6f9dc140
Sean McDermott <mcderm.sean@gmail.com>
2017-03-04 13:48:25 -0800

    improved cell creation algorithm to create 100x100 box in a reasonable amount of time

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 console.log('sanity check');
 
 function createCanvas(rowSize, columnSize) {
+  let pixelWidth = (100 / rowSize)
   for (var i = 0; i < rowSize * columnSize; i++) {
-    let pixelWidth = (100 / rowSize)
     console.log(pixelWidth);
     $('div.canvas').append('<div class="pixel"></div>');
-    $('div.pixel').css('width', `${pixelWidth}%`)
   };
+  $('div.pixel').css('width', `${pixelWidth}%`)
 };
 
 $(document).ready(function () {
-  createCanvas(20, 20);
+  createCanvas(100, 100);
 });
