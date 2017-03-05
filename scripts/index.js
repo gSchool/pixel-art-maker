74cca773f49433fd9d199859bf52f22a942a8f1c
Sean McDermott <mcderm.sean@gmail.com>
2017-03-04 16:26:19 -0800

    added palette circles with potential functionality for different arrays of colors.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,27 @@
 function createCanvas(rowSize, columnSize) {
   let pixelWidth = (100 / rowSize)
-  for (var i = 0; i < rowSize * columnSize; i++) {
-    $('div.canvas').append('<div class="pixel"></div>');
+  for (var i = 1; i <= rowSize * columnSize; i++) {
+    $('div.canvas').append(`<div id="px-${i}" class="pixel"></div>`);
   };
   $('div.pixel').css('width', `${pixelWidth}%`)
 };
 
+function createPalette(palette) {
+  for (var i = 0; i < 6; i++) {
+    $('div.palette').append(`<div id="c-${i}" class="color-choice"></div>`);
+    $(`#c-${i}`).css('background', palette[i])
+  }
+};
+
 $(document).ready(function () {
-  createCanvas(200, 200);
+  let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+  createPalette(colors);
+  createCanvas(16, 9);
 });
+
+//TBD determine pixel's color
+
+// change pixel's color on click
+$('.canvas').on('click', '.pixel', function () {
+  $(this).css('background-color', 'red')
+})
