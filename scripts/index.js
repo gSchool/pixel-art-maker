19d78955c27405dc2957b8a43eb393b7f68c138c
Sean McDermott <mcderm.sean@gmail.com>
2017-03-05 18:36:15 -0800

    added color wheel. needs styling but it works

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 function createCanvas(rowSize, columnSize) {
   let pixelWidth = (100 / rowSize)
+
   for (var i = 1; i <= rowSize * columnSize; i++) {
     $('div.canvas').append(`<div id="px-${i}" class="pixel"></div>`);
   };
@@ -10,7 +11,9 @@ function createPalette(palette) {
   for (var i = 0; i < palette.length; i++) {
     $('div.palette').append(`<div id="c-${i}" class="color-choice"></div>`);
     $(`#c-${i}`).css('background', palette[i])
-  }
+  };
+  $('div.palette').append('<div class="color-wheel"><input id="color-wheel" type="color"/></div>');
+
 };
 
 $(document).ready(function () {
@@ -19,9 +22,14 @@ $(document).ready(function () {
   createCanvas(16, 9);
 });
 
-//determine pixel's color
+// determine pixel's color from preset
 $('.palette').on('click', '.color-choice', function () {
   let $color = $(this).css('background');
+  $('.current-color').css('background', $color);
+})
+// determine pixel's color from wheel
+$('.palette').on('mousedown', '#color-wheel', function () {
+  let $color = $(this).val();
   $('.current-color').css('background', $color);
 })
 
@@ -33,7 +41,7 @@ $(document).mousedown(function () {
   isDragging = false
 });
 
-function changeColor (selector) {
+function changeColor(selector) {
   $(selector).css({
     'background': $('.current-color').css('background'),
     'border-color': 'rgba(182, 182, 182, 0)',
@@ -42,11 +50,9 @@ function changeColor (selector) {
 
 $('.canvas').on('mouseenter', '.pixel', function () {
   if (isDragging) changeColor(this)
-  }).on('click', '.pixel', function () {
-    changeColor(this)
-  })
-
-
+}).on('click', '.pixel', function () {
+  changeColor(this)
+})
 
 
 
