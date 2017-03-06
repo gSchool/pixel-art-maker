d90f961ab7755dcd4805b586c226448cec91117e
Doubleshot1122 <skibo1122@yahoo.com>
2017-03-05 20:24:11 -0800

    base assignment complete, no bonuses yet

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("js loaded");
+let currentColor = "white";
+let paintColor = "blue";
+
+(function buildGrid() {
+  for (var i = 0; i < 1000; i++) {
+    $(".main-frame").append("<div></div>");
+  }
+}());
+
+
+function updateCurrentColorBox(color) {
+  $(".current").css("background-color", color);
+}
+
+$(".colors div").on("click", function () {
+  var bgColor = $(this).css("background-color");
+  paintColor = bgColor;
+  updateCurrentColorBox(bgColor);
+});
+
+$(".main-frame").on("click", 'div', function (e) {
+  $(this).css("background-color", paintColor);
+});
