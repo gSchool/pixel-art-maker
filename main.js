b3344b7881fe123e0ea162ec8cb3a0f96d3a3c9c
Cheryln <cherylnbarber@gmail.com>
2017-03-20 16:47:48 -0700

    refactored js code

--- main.js
+++ main.js
@@ -1,285 +1,97 @@
 
 
-
-for (var i = 1; i <= 3000; i++) {
-  var $gridBox = $('<div class="grid"></div>');
-  $('.gridWrapper').append($gridBox);
-}
-
-for (var j = 1; j <= 750; j++) {
-  var $gridBox = $('<div class="grid1"></div>');
-  $('.gridWrapper').append($gridBox);
-}
-
-for (var k = 1; k <= 600; k++) {
-  var $gridBox = $('<div class="grid2"></div>');
-  $('.gridWrapper').append($gridBox);
-}
-
-$('#sizeSelector').on('click', function(){
-  $('div').removeClass('grid1').addClass('grid');
-  $('div').removeClass('grid2').addClass('grid');
-});
-
-$('#sizeSelector1').on('click', function(){
-  $('div').removeClass('grid').addClass('grid1');
-  $('div').removeClass('grid2').addClass('grid1');
-})
-
-$('#sizeSelector2').on('click', function(){
-$('div').removeClass('grid').addClass('grid2');
-$('div').removeClass('grid1').addClass('grid2');
-});
-
-
-
-
-
-
-
 var isDown = false; // Tracks status of mouse button
 
-  $(document).mousedown(function() {
-    isDown = true; // When mouse goes down, set isDown to true
-  })
-  .mouseup(function() {
-    isDown = false; // When mouse goes up, set isDown to false
-  });
-
-
-// ******************   Blue Button  *****************
-
-$('#button1').on('click', function(){
-  var $blue = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $blue);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $blue);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $blue);
-    }
-  });
+$(document).mousedown(function() {
+  isDown = true; // When mouse goes down, set isDown to true
+})
+.mouseup(function() {
+  isDown = false; // When mouse goes up, set isDown to false
 });
 
-// ******************   Orang Button  *****************
+selectGridSize ();
 
-$('#button2').on('click', function(){
-  var $orange = $(this).css('background-color');
+useSelectedColorButton ('#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8', '.grid, .grid1, .grid2');
 
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $orange);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $orange);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $orange);
-    }
-  });
-});
-
-// ******************   Green Button  *****************
-
-$('#button3').on('click', function(){
-  var $green = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $green);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $green);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $green);
-    }
-  });
-});
-
-// ******************   yellow Button  *****************
-
-$('#button4').on('click', function(){
-  var $yellow = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $yellow);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $yellow);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $yellow);
-    }
-  });
-});
-
-// ******************   pink Button  *****************
-
-$('#button5').on('click', function(){
-  var $pink = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $pink);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $pink);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $pink);
-    }
-  });
-});
-
-// ******************   Grey Button  *****************
-
-$('#button6').on('click', function(){
-  var $grey = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $grey);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $grey);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $grey);
-    }
-  });
-});
-
-// ******************   Black Button  *****************
-
-$('#button7').on('click', function(){
-  var $black = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $black);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $black);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $black);
-    }
-  });
-});
-
-// ******************   Red Button  *****************
-
-$('#button8').on('click', function(){
-  var $red = $(this).css('background-color');
-
-  $(".grid").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $red);
-    }
-  });
-  $(".grid1").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $red);
-    }
-  });
-  $(".grid2").mouseover(function(){
-    if(isDown) {    // Only change css if mouse is down
-      $(this).css('background-color', $red);
-    }
-  });
-});
-
-// ******************   Color Picker  *****************
-
-$(".colorPicker").on("change",function(){
-  var $color = $('.colorPicker').val();
-// $(".selected").css("background-color", $color);
+useColorPickerColor('.grid, .grid1, .grid2');
 
 
-  $(".grid").mouseover(function(){
-    if(isDown) {  // Only change css if mouse is down
-       $(this).css('background-color', $color);
-    }
-  });
+changeBackgroundColor('.grid, .grid1, .grid2', '#backgroundColor1', 'lightpink', 'beige', 'white', 'lightblue');
+changeBackgroundColor('.grid, .grid1, .grid2', '#backgroundColor2', 'lightblue', 'beige', 'white', 'lightpink');
+changeBackgroundColor('.grid, .grid1, .grid2', '#backgroundColor3', 'lightpink', 'lightblue', 'white', 'beige');
+changeBackgroundColor('.grid, .grid1, .grid2', '#backgroundColor4', 'lightpink', 'beige', 'lightblue', 'white');
 
-  $(".grid1").mouseover(function(){
-    if(isDown) {  // Only change css if mouse is down
-       $(this).css('background-color', $color);
-    }
-  });
-
-  $(".grid2").mouseover(function(){
-    if(isDown) {  // Only change css if mouse is down
-       $(this).css('background-color', $color);
-    }
-  });
-});
-
-// *********************************************************
-
-$('.clearButton').click(function() {
-    location.reload();
-});
+clearCanvas();
 
 
 
-// ***********Testing local storage**************
-//not working
+// =============== Functions =========================
 
-localStorage.myCanvas = JSON.stringify($('#gridWrap'));
+function selectGridSize (){
 
-function init(){
-if(localStorage.myCanvas){
-  $oldInfo =  JSON.parse(localStorage.myCanvas);
-  $('gridWrap').html($oldInfo);
+  for (var i = 1; i <= 3000; i++) {
+    var $gridBox = $('<div class="grid"></div>');
+    $('.gridWrapper').append($gridBox);
   }
+
+  for (var j = 1; j <= 750; j++) {
+    var $gridBox = $('<div class="grid1"></div>');
+    $('.gridWrapper').append($gridBox);
+  }
+
+  for (var k = 1; k <= 600; k++) {
+    var $gridBox = $('<div class="grid2"></div>');
+    $('.gridWrapper').append($gridBox);
+  }
+
+  $('#sizeSelector').on('click', function(){
+    $('div').removeClass('grid1 grid2').addClass('grid');
+  });
+
+  $('#sizeSelector1').on('click', function(){
+    $('div').removeClass('grid grid2').addClass('grid1');
+  });
+
+  $('#sizeSelector2').on('click', function(){
+    $('div').removeClass('grid grid1').addClass('grid2');
+  });
 }
-//********* end local storage testing ***************
 
+function backgroundColor(grid, col1, col2, col3, addCol){
+  $(grid).removeClass(`${col1} ${col2} ${col3}`).addClass(`${addCol}`);
+}
 
-$('#backgroundColor1').on('click', function(){
-  $('.grid').addClass("lightblue").removeClass('lightpink beige white');
-});
-$('#backgroundColor2').on('click', function(){
-  $('.grid').addClass("lightpink").removeClass('lightblue beige white');
-});
-$('#backgroundColor3').on('click', function(){
-  $('.grid').addClass("beige").removeClass('lightblue lightpink white');
-});
-$('#backgroundColor4').on('click', function(){
-  $('.grid').addClass("white").removeClass('lightblue lightpink beige');
-});
+function changeBackgroundColor(grid, button, col1, col2, col3, addCol){
+  $(button).on('click', function(){
+    backgroundColor(grid, col1, col2, col3, addCol);
+  });
+}
+
+function useSelectedColorButton (button, grid){
+  $(button).on('click', function(){
+    var color = $(this).css('background-color');
+
+    $(grid).mouseover(function(){
+      if(isDown) {    // Only change css if mouse is down
+        $(this).css('background-color', color);
+      }
+    });
+  });
+}
+
+function useColorPickerColor (grid){
+  $('.colorPicker').on('change',function(){
+    var $color = $('.colorPicker').val();
+
+    $(grid).mouseover(function(){
+      if(isDown) {$(this).css('background-color', $color);
+      }
+    });
+  });
+}
+
+function clearCanvas (){
+  $('.clearButton').click(function() {
+    location.reload();
+  });
+}
