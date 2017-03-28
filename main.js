28c3e4b906aae5d7e73f366a5f3cd2d8ce14f81e
Jules Perryman <jules.perryman@gmail.com>
2017-03-28 16:47:19 -0600

    Made some modifications

--- main.js
+++ main.js
@@ -1,88 +1,107 @@
-
-
-
-
 var container = document.querySelector('.container')
 
 
-container.addEventListener('click', function(event) {
-	console.log(event.target)
-
-  if (event.target.classList.contains('grid')) {
-  	event.target.style.backgroundColor = color
-  }
-
-
-})
-
-var cellCount = 1600
+// DEFINE GRID AND AMOUNT OF SQUARES
+var cellCount = 2100
 for (var i = 1; i <= cellCount; i++) {
-  var newCell = document.createElement('div')
-  newCell.classList.add('grid')
-  // newCell.textContent =
-  container.appendChild(newCell)
+    var newCell = document.createElement('div')
+    newCell.classList.add('grid')
+    // newCell.textContent =
+    container.appendChild(newCell)
 }
 
 
-var color = "#ffffff";
+//COLOR THE INDIVIDUAL GRID
+container.addEventListener('mousedown', function(event) {
+    console.log(event.target)
+    if (event.target.classList.contains('grid')) {
+        event.target.style.backgroundColor = color
+    }
+})
+
+// COLOR WHILE MOUSE IS HELD DOWN
+container.addEventListener('mouseover', function(event) {
+  if(event.target.classList.contains('grid') && event.buttons === 1) {
+    event.target.style.backgroundColor = color
+  }
+})
 
 
+
+// COLOR PALLETE & COLORS
+var color = "lightblue";
 
 var paletteBlue = document.querySelector("#blue");
 var paletteRed = document.querySelector("#red");
 var paletteWhite = document.querySelector("#white");
 var paletteGreen = document.querySelector("#green");
 var paletteYellow = document.querySelector("#yellow");
+var paletteLightblue = document.querySelector("#lightblue");
 var palettePurple = document.querySelector("#purple");
+var paletteOrange = document.querySelector("#orange");
+var paletteLightgreen = document.querySelector("#lightgreen");
+var paletteIndianred = document.querySelector("#indianred");
+var paletteHotpink = document.querySelector("#hotpink");
 var paletteBlack = document.querySelector("#black");
 
 
-paletteBlue.addEventListener("click", function(){
- color = "blue";
- console.log("blue");
+paletteBlue.addEventListener("click", function() {
+    color = "blue";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+	
+})
+
+paletteRed.addEventListener("click", function() {
+    color = "red";
     document.querySelector('.currentColor').style.backgroundColor = color;
 })
 
-paletteYellow.addEventListener("click", function(){
- color = "yellow";
- document.querySelector('.currentColor').style.backgroundColor = color;
-
- console.log("yellow");
+paletteWhite.addEventListener("click", function() {
+    color = "white";
+    document.querySelector('.currentColor').style.backgroundColor = color;
 })
 
-paletteWhite.addEventListener("click", function(){
- color = "white";
- document.querySelector('.currentColor').style.backgroundColor = color;
-
- console.log("white");
-
-})
-paletteRed.addEventListener("click", function(){
- color = "#e32020 ";
- document.querySelector('.currentColor').style.backgroundColor = color;
-
- console.log("red");
-
+paletteGreen.addEventListener("click", function() {
+    color = "green";
+    document.querySelector('.currentColor').style.backgroundColor = color;
 })
 
-paletteGreen.addEventListener("click", function(){
- color = "#2ae831";
- document.querySelector('.currentColor').style.backgroundColor = color;
-
- console.log("green");
+paletteYellow.addEventListener("click", function() {
+    color = "yellow";
+    document.querySelector('.currentColor').style.backgroundColor = color;
 })
 
-
-palettePurple.addEventListener("click", function(){
- color = "purple";
- document.querySelector('.currentColor').style.backgroundColor = color;
-
- console.log("purple");
+paletteLightblue.addEventListener("click", function() {
+    color = "lightblue";
+    document.querySelector('.currentColor').style.backgroundColor = color;
 })
 
-paletteBlack.addEventListener("click", function(){
- color = "black";
- document.querySelector('.currentColor').style.backgroundColor = color;
+palettePurple.addEventListener("click", function() {
+    color = "purple";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+})
 
- console.log("black");
+paletteOrange.addEventListener("click", function() {
+    color = "orange";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+})
+
+paletteLightgreen.addEventListener("click", function() {
+    color = "lightgreen";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+})
+
+paletteIndianred.addEventListener("click", function() {
+    color = "indianred";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+})
+
+paletteHotpink.addEventListener("click", function() {
+    color = "hotpink";
+    document.querySelector('.currentColor').style.backgroundColor = color;
+})
+
+paletteBlack.addEventListener("click", function() {
+    color = "black";
+    document.querySelector('.currentColor').style.backgroundColor = color;
 })
