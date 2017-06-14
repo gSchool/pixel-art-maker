e43539ef885e209bf7fd0126c0e475b468f8c9e6
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-13 22:48:18 -0700

    got multicolors which is super sick, but my CSS is hosed

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let pixels = document.getElementsByClassName("pixel");
+let colorDot = document.getElementsByClassName("color_dot")
+let chosenColor = ""
 
 for (let i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener("click", paint);
+  pixels[i].addEventListener("click", paint);
+}
+for (let i = 0; i < pixels.length; i++) {
+  colorDot[i].addEventListener("click", colorChoice);
+}
+
+function colorChoice(event){
+  chosenColor = event.target.id;
+  // console.log(chosenColor);
 }
 
 function paint(event){
-  event.target.style.backgroundColor = "red";
+  event.target.style.backgroundColor = chosenColor;
 }
