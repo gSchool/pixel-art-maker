1396b55ab3c59897251f3c01c29560f1edae0076
Heather Arvidson <heather.arvidson@gmail.com>
2017-06-19 21:27:06 -0700

    finishing touches

--- pixel.js
+++ pixel.js
@@ -1,6 +1,6 @@
 // create grid; takes two arguments, both numbers are units of pixel boxes
 const grid = document.querySelector('.grid');
-const horizontalPixels = 30;
+const horizontalPixels = 40;
 const verticalPixels = 60;
 
 function makeGrid() {
@@ -17,13 +17,19 @@ function makeGrid() {
   }
  }
 
+ function clearGrid() {
+   grid.innerHTML = "";
+   makeGrid();
+ }
+
+
+
 makeGrid();
 
 let gridSize = function() {
   let dimensions = {};
   dimensions.width = horizontalPixels * 5;
   dimensions.height = verticalPixels * 5;
-  console.log(dimensions);
   return dimensions;
 }
 
@@ -31,12 +37,15 @@ grid.setAttribute("style",`width:${gridSize.width}px;`);
 grid.setAttribute("style",`height:${gridSize.height}px;`);
 
 // create palette
-const colors = ["DarkRed","FireBrick","IndianRed","Red","Crimson","OrangeRed","Tomato","Coral","Salmon","MediumVioletRed","PaleVioletRed","Orchid","Plum","Fuchsia","DeepPink","HotPink","Pink","MediumOrchid","MediumPurple","Lavender","Thistle","DarkViolet","Purple","RoyalBlue","MediumSlateBlue","SlateBlue","DarkOrchid","Indigo","Navy","DarkSlateBlue","MediumBlue","Blue","DeepSkyBlue","DodgerBlue","SteelBlue","CornflowerBlue","SkyBlue","PowderBlue","Aqua","Turquoise","MediumTurquoise","DarkTurquoise","PaleTurquoise","Aquamarine","MediumSpringGreen","Teal","DarkCyan","CadetBlue","MediumAquaMarine","DarkSeaGreen","MediumSeaGreen","SeaGreen","ForestGreen","DarkOliveGreen","Olive","LimeGreen","YellowGreen","SpringGreen","PaleGreen","Chartreuse","GreenYellow","Yellow","Gold","Khaki","Moccasin","PeachPuff","GoldenRod","Orange","Darkorange","SandyBrown","DarkSalmon","BurlyWood","Chocolate","RosyBrown","Sienna","SaddleBrown","DarkSlateGray","SlateGray","Gainsboro","White","Black"];
+const colors = ["DarkRed","FireBrick","IndianRed","Red","Crimson","OrangeRed","Tomato","Coral","Salmon","MediumVioletRed","PaleVioletRed","Orchid","Plum","Fuchsia","DeepPink","HotPink","Pink","MediumOrchid","MediumPurple","Lavender","Thistle","Purple","DarkOrchid","MediumSlateBlue","SlateBlue","Indigo","Navy","RoyalBlue","Blue","DeepSkyBlue","CornflowerBlue","SkyBlue","PowderBlue","Aqua","Turquoise","DarkTurquoise","PaleTurquoise","Aquamarine","MediumAquaMarine","Teal","DarkCyan","CadetBlue","DarkSeaGreen","MediumSeaGreen","SeaGreen","ForestGreen","DarkOliveGreen","Olive","YellowGreen","LimeGreen","MediumSpringGreen","SpringGreen","Chartreuse","PaleGreen","GreenYellow","Yellow","Gold","Moccasin","GoldenRod","Orange","Darkorange","DarkSalmon","BurlyWood","RosyBrown","Chocolate","Sienna","SaddleBrown","DarkSlateGray","SlateGray","Gainsboro","White","Black"];
 
 const palette = document.querySelector('.palette');
 const current = document.createElement('div');
+const clearButton = document.querySelector('button');
 let brushColor = current.style.backgroundColor;
 let isPainting = false;
+
+
 
 function createPalette() {
   for (let i = 0; i < colors.length; i++) {
@@ -49,38 +58,30 @@ function createPalette() {
 }
 
 // change color of pixels on canvas
-  function addColor(event) {
+function addColor(event) {
+    if (event.target.classList.contains('pixel')) {
       event.target.style = `background-color: ${brushColor}`;
-  }
-
-  function changeBrushColor(event) {
-    brushColor = event.target.id;
-    current.style.backgroundColor = brushColor;
-    console.log(`brush color is ${brushColor}`);
-  }
-
-  function startPainting(event) {
-    isPainting = true;
-    event.target.style = `background-color: ${brushColor}`;
-  }
-
-  function painting(event) {
-    if (isPainting === true) {
-      event.target.style = `background-color: ${brushColor}`;
-      console.log(event.target);
     }
-  };
+}
 
-  function endPainting(event) {
-    isPainting = false;
-  };
+function changeBrushColor(event) {
+  brushColor = event.target.id;
+  current.style.backgroundColor = brushColor;
+}
 
+function painting(event) {
+  if (event.target.classList.contains('pixel')) {
+    if (event.buttons) {
+      event.target.style = `background-color: ${brushColor}`;
+    }
+  }
+}
 
-  current.classList.add('current');
-  palette.appendChild(current);
-  createPalette();
-  grid.addEventListener('click', addColor);
-  grid.addEventListener('mousedown', startPainting);
-  grid.addEventListener('mouseenter', painting, false);
-  grid.addEventListener('mouseup', endPainting);
-  palette.addEventListener('click', changeBrushColor);
+current.classList.add('current');
+palette.appendChild(current);
+createPalette();
+palette.appendChild(clearButton);
+grid.addEventListener('click', addColor);
+grid.addEventListener('mouseover', painting);
+palette.addEventListener('click', changeBrushColor);
+clearButton.addEventListener('click', clearGrid);
