f8adafc57950ae775eb5d80ad3f85175175dced3
ThomasElkins <elkins.thomas@gmail.com>
2017-07-09 21:16:16 -0700

    fully working, needs UI tweaks

--- pixelArt.js
+++ pixelArt.js
@@ -1,41 +1,27 @@
 $(function() {
 
-//Complete this function to autofill grid
-// function fillGrid(){
-//   var fillBox = $('.abox')
-//     for (var i = 1; i < 100; i++){
-//       $('#artContainer').append(<div class="abox"></div>)
-//     }
-// }
-// fillGrid();
+// Autofill art grid
+function fillGrid(){
+    for (var i = 0; i < 860; i++){
+      $('#artContainer').append('<div class="abox"></div>')
+    }
+}
+fillGrid();
 
 
 var currentColor;
-// Brush and Eraser
-$('.activeColor').append('<h3>Brush</h3>')
+// Brush and Eraser and reset
+$('#brushColor').append('<h3>Brush</h3>')
 $('#eraser').append('<h3>Eraser</h3>')
+$('#reset').append('<h3>Clear</h3>')
 
 // Pick a color
 $('.pbox').click(function(){
   currentColor = $(this).css('background-color')
-  $('.activeColor').css('background-color', currentColor)
+  $('#brushColor').css('background-color', currentColor)
 })
 
-// // Paint a color
-// $('.abox').click(function(){
-//   $(this).css('background-color', currentColor)
-//   // Reinstate this for full coloring
-//   // $(this).css('border-color', currentColor)
-// })
-
-// Drag for color
-// $('.abox').dblclick(function dragPaint(){
-//     $(this).css('background-color', currentColor)
-//         $('.abox').mouseenter(function(){
-//             $(this).css('background-color', currentColor)
-//           })
-//         })
-
+//Paint and Drag
 $('.abox').mousedown(function() {
     $(".abox").bind('mouseover',function(){
         $(this).css("background-color", currentColor);
@@ -49,7 +35,13 @@ $('.abox').mousedown(function() {
   $(this).css("background-color", currentColor);
 });
 
+//Clear the canvas
 
+$('#reset').click(function(){
+  $('.abox').each(function(){
+    $(this).css('background-color', 'lightgray')
+  })
+})
 
 
 
