fca9c1d0b7f26dd0577c13a5e03605ca844ad02e
Nathan Daniel Peterson <natperson@gmail.com>
2017-09-14 13:44:02 -0700

    increase size of canvas and colors

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 console.log("yo!")
 'use strict'
 
+function addListenerMulti(element, eventNames, listener) {
+  var events = eventNames.split(' ');
+  for (var i=0, iLen=events.length; i<iLen; i++) {
+    element.addEventListener(events[i], listener, false);
+  }
+}
+
+addListenerMulti(window, 'mousemove touchmove', function(){…});
+
+function brushWork(){
+  this.target.style.background = colorchoice
+}
+
 let pixel = document.querySelectorAll('.pixel');
 for(let i = 0; i < pixel.length;i++){
   let canvasUnit = pixel[i]
   canvasUnit.addEventListener('click', function(e){
-    e.target.style.background = colorChoice
+        e.target.style.background = colorChoice
   })
 }
 
