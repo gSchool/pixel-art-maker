8f162092f42c8f3fc214b1aa5b39cd21d45e34f1
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-06-10 13:15:37 +0530

    colors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for(let y = 0; y < HEIGHT; y++) {
 document.body.appendChild(canvas);
 
 // Palette
-const colors = ['red', 'blue'];
+const colors = ['#b23232', '#ff4848', '#ff6c6c', '#e59b40', '#ffad48', '#ffc57e', '#e5de40', '#fff748', '#fffa91', '#39cc4b', '#48ff5e', '#91ff9e', '#3248b2', '#4867ff', '#91a3ff', '#6432b2', '#8f48ff', '#bb91ff', '#7c2b99', '#cf48ff', '#e291ff', '#000000', '#323232', '#666666', '#999999', '#cccccc', '#ffffff', '#3a2119', '#512e23', '#754233', '#90675b', '#ac8d84'];
 const palette = document.createElement('div');
 palette.className = 'palette';
 for(let color of colors) {
