7ff3bbd0e1f41e5871e22788564498e9f8676e70
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-06-10 12:46:13 +0530

    Color swatches

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const WIDTH = 4; //64;
 const HEIGHT = 4; //32;
 
+// Canvas
+const canvas = document.createElement('div');
+canvas.className = 'canvas';
 for(let y = 0; y < HEIGHT; y++) {
     const row = document.createElement('div');
     row.className = 'row';
@@ -12,5 +15,18 @@ for(let y = 0; y < HEIGHT; y++) {
         });
         row.appendChild(pixel);
     }
-    document.body.appendChild(row);
+    canvas.appendChild(row);
 }
+document.body.appendChild(canvas);
+
+// Palette
+const colors = ['red', 'blue'];
+const palette = document.createElement('div');
+palette.className = 'palette';
+for(let color of colors) {
+    const swatch = document.createElement('div');
+    swatch.className = 'swatch';
+    swatch.style.backgroundColor = color;
+    palette.appendChild(swatch);
+}
+document.body.appendChild(palette);
