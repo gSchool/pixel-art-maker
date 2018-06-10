6de40658a09596c79eabc4ddbb8f21274d255a65
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-06-10 12:29:56 +0530

    Pixel grid

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.body.innerHTML = "Hello world!";
+const WIDTH = 4; //64;
+const HEIGHT = 4; //32;
+
+for(let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    for(let x = 0; x < WIDTH; x++) {
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        row.appendChild(pixel);
+    }
+    document.body.appendChild(row);
+}
