54b15a300374ba1a0a1b6684db9bdd8917b648fe
Rup Chitrak <rchitrak10@gmail.com>
2018-06-11 16:00:58 +0530

    2X2 grid features

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let main = document.createElement('div');
 main.className = 'divContainer';
 document.body.appendChild(main);
 
-for(let i=0;i<2;i++) {
+for(let i=0;i<20;i++) {
     const rowDiv=document.createElement('div');
-    rowDiv.className = 'rowDivContainer'+i
+    rowDiv.className = 'rowDivContainer';
     main.appendChild(rowDiv);
 
-    for(let j=0;j<2;j++) {
+    for(let j=0;j<20;j++) {
         const div=document.createElement('div');
         //div.innerHTML = 'red';
         div.className='divElem';
