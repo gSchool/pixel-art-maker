00629d2aef87e969eebc28b632f3b5a1c36bae1d
Rup Chitrak <rchitrak10@gmail.com>
2018-06-11 15:45:38 +0530

    2X2 grid

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.body.innerHTML = "Hello world!";
+// document.body.innerHTML = "Hello world!";
+
+let main = document.createElement('div');
+main.className = 'divContainer';
+document.body.appendChild(main);
+
+for(let i=0;i<2;i++) {
+    const rowDiv=document.createElement('div');
+    rowDiv.className = 'rowDivContainer'+i
+    main.appendChild(rowDiv);
+
+    for(let j=0;j<2;j++) {
+        const div=document.createElement('div');
+        //div.innerHTML = 'red';
+        div.className='divElem';
+        div.addEventListener("click",(e)=>{
+            e.target.className='colorChange';
+            console.log('e------->',e.target.innerHTML);
+        });
+        rowDiv.appendChild(div);
+    }
+}
