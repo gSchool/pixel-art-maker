64232da582e7662217b998b123d12a75fb02fe31
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-02-07 14:19:26 -0800

    Completed basic project requirements

---
+++ app.js
@@ -0,0 +1,34 @@
+(function() {
+    "use strict";
+
+    let canvas = document.getElementById('canvas');
+    let palette = document.getElementById('palette');
+    let colorInd = document.getElementById('colorIndicator')
+    let currColor;
+    for (var i = 0; i < 256; i++) {
+        let elem = document.createElement('div');
+        elem.className = 'pixel';
+        canvas.append(elem);
+    }
+
+    palette.onclick = function(event) {
+        let target = event.target;
+        if (target.className === 'paletteItem') {
+            currColor = target.style.backgroundColor;
+            colorInd.style.backgroundColor = currColor;
+            console.log('working ' + currColor);
+        }
+    }
+
+    canvas.onclick = function(event) {
+        let target = event.target;
+        if (target.classList[0] === 'pixel') {
+            changeColor(target);
+        }
+    }
+
+    function changeColor(target) {
+        target.style.backgroundColor = currColor;
+    }
+
+})();
