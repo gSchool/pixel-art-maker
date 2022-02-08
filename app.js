b9596508e070f4568e4b6b1d5baba8afdc25b639
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-02-08 12:50:24 -0800

    Completed bonus 1

--- app.js
+++ app.js
@@ -5,34 +5,51 @@
     let palette = document.getElementById('palette');
     let colorInd = document.getElementById('colorIndicator')
     let currColor;
+    let mouseState = false;
+
     for (var i = 0; i < 256; i++) {
         let elem = document.createElement('div');
         elem.className = 'pixel';
         canvas.append(elem);
+
+        elem.addEventListener('mousedown', function() {
+            mouseState = true;
+            changeColor(elem);
+        })
+        elem.addEventListener('mouseenter', function() {
+            if (mouseState) {
+                changeColor(elem);
+            }
+        })
+        elem.addEventListener('mouseup', function() {
+            mouseState = false;
+        })
     }
 
-    var mouseDown = false;
-    document.body.onmousedown = function() {
-        mouseDown = true;
-    }
-    document.body.onmouseup = function() {
-        mouseDown = false;
-    }
 
-    if (mouseDown && )
+    // canvas.onmousedown = function(event) {
+    //     mouseState = true;
+    //     canvas.onmouseenter = function(event) {
+    //         let target = event.target;
+    //         console.log(target);
+    //         if (target.className === 'pixel') {
+    //             console.log('working');
+    //         }
+    //     }
+    // }
 
-        palette.onclick = function(event) {
+
+    palette.onclick = function(event) {
         let target = event.target;
         if (target.className === 'paletteItem') {
             currColor = target.style.backgroundColor;
             colorInd.style.backgroundColor = currColor;
-            console.log('working ' + currColor);
         }
     }
 
     canvas.onclick = function(event) {
         let target = event.target;
-        if (target.classList[0] === 'pixel') {
+        if (target.className === 'pixel') {
             changeColor(target);
         }
     }
