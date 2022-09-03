d9990683b4fb421b2655d10821e9ff6eb5821983
Segerlu <steven.segerlund@yahoo.com>
2022-09-02 22:17:32 -0400

    all functionality for base requirements added. may need to adjust for different screens

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 let canvas = document.getElementById('canvas');
 let clickedColor = 'white';
 let colorBoard = document.getElementById('colorBoard');
-let arrayOfColors = colorBoard.getElementsByClassName('color');
+let arrayOfColors = Object.values(colorBoard.getElementsByClassName('color'));
+
 function makeDivs(){
     let pix = document.createElement('div');
     pix.className = 'pixel';
-    pix.style.width = '1em';
-    pix.style.height = '1em';
+    pix.style.width = '0.5em';
+    pix.style.height = '0.5em';
     pix.style.border = '.5px dotted #4713a3';
     pix.style.backgroundColor = 'black';
     pix.style.display = 'flex' ;
     pix.style.flexWrap = 'wrap';
     return pix;
 }
-console.log(canvas)
-for (var i = 0; i < 1537; i++){
+for (var i = 0; i < 5880; i++){
     var makePix = makeDivs();
     canvas.append(makePix);
 }
 canvas.addEventListener('click', (e) => {
     if (e.target.className === 'color'){
+        
         clickedColor = e.target.style.backgroundColor;
 
-        Object.values(arrayOfColors).forEach((i)=>{
+        arrayOfColors.forEach((i)=>{
             if (clickedColor === i.style.backgroundColor){
                 i.style.border = '3px solid white';
             } else {
                 i.style.border = '';
             }
         })
+    } else if (e.target.className === "pixel") {
+        e.target.style.backgroundColor = clickedColor;
     }
 }) 
